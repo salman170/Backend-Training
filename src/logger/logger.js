165b04c45c212d662f8c19de70f092aac113046f
salman170 <salmanansari02120@gmail.com>
2022-08-06 13:49:58 +0530

    All assignment

--- src/logger/logger.js
+++ src/logger/logger.js
@@ -10,19 +10,3 @@ module.exports.Greeting = Greeting
 
 
 
-// Problem 2
-// Module 2 : src/util/helper.js
-
-// - printDate() : prints the current date
-// - printMonth() : prints the current month
-// - getBatchInfo() : prints batch name, week#, Day#, the topic being taught today is ….. For example - Radon, W3D3, the topic for today is Nodejs module system’
-	
-// 	Call all these functions in route.js inside the test-me route handler
-
-// Problem 3
-// Module 3: src/validator/formatter.js
-// - trim() : calls the trim function on a hardcoded string for example ‘ functionUp  ’
-// - changetoLowerCase() : changes the case of the string to lower. [Call toLowerCase() on a hardcoded string]
-// - changeToUpperCase() : changes the case of the string to upper case [Call toUpperCase() on a hardcoded string]
-
-// Call all these functions in route.js inside the test-me route handler
