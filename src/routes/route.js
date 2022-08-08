81e094f6a00e12d3e97bd4588c64c307526d417a
salman170 <salmanansari02120@gmail.com>
2022-08-08 14:54:45 +0530

    Assignment4

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const _ = require('lodash')
 const abc = require('../introduction/intro')
 const ass1 = require('../logger/logger')
 const ass2 = require('../util/helper')
@@ -6,9 +7,34 @@ const ass3 = require('../validator/formatter')
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
-    ass1.Greeting()
-    ass2.printDate()
-    ass3.fun()
+    // ass1.Greeting()
+    // ass2.printDate()
+    // ass3.fun()
+    let mL = ['January', 'February',
+     'March', 'April', 'May', 'June', 
+     'July', 'August', 'September', 'October', 
+     'November', 'December'];
+     mL= _.chunk(mL,3)
+     console.log(mL)
+
+     let odd = [1,3,5,7,9,11,13,15,17,19]
+     odd = _.tail(odd)
+     console.log(odd)
+
+     const arry1 = [1, 2, 1, 3, 4, 3, 5];
+     const arry2 = [1, 2, 10, 4, 311, 5];
+     const arry3 = [121, 2, 1, 30, 4, 3, 521];
+     const arry4 = [1, 2055, 1, 3, 4, 30, 5];
+     const arry5 = [1, 20, 1, 3, 45454, 3, 5];
+     const arr = _.union(arry1,arry2,arry3,arry4,arry5);
+     console.log(arr);
+
+     const object = _.fromPairs([['horror','The Shining'],
+     ['drama','Titanic'],
+     ['thriller','Shutter Island'],
+     ['fantasy','Pans Labyrinth']])
+     console.log(object)
+
     res.send('My second ever api!')
 });
 
