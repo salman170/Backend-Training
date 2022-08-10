2d8633bf9ab0dd57155c79519e58f42f021768f6
salman170 <salmanansari02120@gmail.com>
2022-08-10 12:32:21 +0530

    Assignment 5- completed

--- src/routes/route.js
+++ src/routes/route.js
@@ -13,6 +13,35 @@ router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
 });
 
+router.get('/sol1', function (req, res) {
+    let aa = [1, 2, 3, 5, 6, 7]
+    let total = 0
+    for (let i in aa) {
+        total += aa[i]
+    }
+
+    let sum = aa.pop()
+    let sumOfarray = sum * (sum + 1) / 2
+    let b = sumOfarray - total
+    res.send({"Missing No is" :b })
+
+})
+router.get('/sol2', function (req, res) {
+    let aa = [33,34,35,37,38]
+    let len = aa.length
+    let total = 0
+    for (let i in aa) {
+        total += aa[i]
+    }
+
+    let first = aa[0]
+    let sum = aa.pop()
+    let sumOfarray = (len + 1) * (first + sum) / 2
+    let b = sumOfarray - total
+    res.send({ "Missing No is" :b })
+
+})
+
 
 
 router.get("/movies/:indexNumber", function (req, res) {
