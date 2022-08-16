464131ce4c978cfbb3fb111aee647d4cf5a8c6c6
salman170 <salmanansari02120@gmail.com>
2022-08-16 23:52:57 +0530

    Assignment

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,20 +1,29 @@
 const mongoose = require('mongoose');
 
-const bookSchema = new mongoose.Schema( {
-    bookName: String, 
-    authorName: String, 
-    tags: [String],
-    
+const bookSchema = new mongoose.Schema({
+    bookName: {
+        type: String,
+        require: true,
+    },
+    tags: [
+        {
+            authorName: String,
+            totalPages: Number,
+            stockAvailable: Boolean,
+        }
+    ],
+
     isPublished: Boolean,
     prices: {
         indianPrice: String,
         europePrice: String,
     },
-    sales: {type: Number, default: 10}
+    year: { type: Number, default: 2021 },
 }, { timestamps: true });
 
 
-module.exports = mongoose.model('Book', bookSchema) //users
+
+module.exports = mongoose.model('BookAssignment', bookSchema) //users
 
 //Validation:
 //require:true
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const router = express.Router();
 // const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
 const BookController= require("../controllers/bookController")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createUser", UserController.createUser  )
 
-router.get("/getUsersData", UserController.getUsersData)
 
-router.post("/createBook", BookController.createBook  )
+router.post("/createBook", BookController.createBook )
+router.post("/bookList", BookController.bookList  )
+router.post("/getBooksInYear", BookController.getBooksInYear  )
+router.post("/getParticularBooks", BookController.getParticularBooks  )
+router.post("/getXINRBooks", BookController.getXINRBooks  )
+router.post("/getRandomBooks", BookController.getRandomBooks )
 
-router.get("/getBooksData", BookController.getBooksData)
 
 module.exports = router;
