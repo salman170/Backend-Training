5b5d7c1d0f45c83246ad8c8ff60c1a96d8f2e703
salman170 <salmanansari02120@gmail.com>
2022-08-24 16:16:00 +0530

    middleware-1

--- src/index.js
+++ src/index.js
@@ -3,21 +3,37 @@ const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
 const app = express();
+const moment = require("moment")
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
-    useNewUrlParser: true
-})
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
 
+
+/* Write a middleware that logs (console.log) some data everytime any API is hit
+Data to be logged:-the current timestamp(as date time) , the IP of the user and the route being requested).
+For this first figure out how to get the route location being request, how to get current timestamp and how to get the IP.
+NOTE: ip of local computer will come as ::1 so dont get disturbed by seeing this)
+ 
+e.g: you should be logging something like this on each line:
+time , IP, Route should be printed on each line in terminal( every time an api is hit)
+2010-08-19 14:00:00 , 123.459.898.734 , /createUser */
+
+// app.use (
+//     function (req, res, next) {
+//         console.log ("inside GLOBAL MW");
+//         next();
+//   }
+//   );
 app.use (
     function (req, res, next) {
-        console.log ("inside GLOBAL MW");
-        next();
+    
+ let timeAndYear = moment().format("YYYY-MM-DD  hh:mm:ss ")
+ 
+    // console.log(req.url);             // relative path with query|search params
+    console.log(timeAndYear+" , "+req.ip +"  , " +req.url)                   
+        next()
   }
   );
 
