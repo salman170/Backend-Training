29f04ebd77dfd1343dac9a76d3917d02e1de636e
salman170 <salmanansari02120@gmail.com>
2022-08-27 10:20:50 +0530

    Assignment done

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -12,12 +12,12 @@ const createOrder = async function (req, res) {
     else if (!product) {
         return res.send({ status: false, msg: "produt validations fail" })
     }
-    if (data["isfreeappuser"]) {
+    if (data["isFreeAppUser"]) {
         req.body.amount = 0;
         let savedData = await orderModel.create(data)
         return res.send({ data: savedData })
     } 
-     if (!data["isfreeappuser"]){
+     if (!data["isFreeAppUser"]){
         let userBalance = await userModel.findById(data["userId"]).select({ balance: 1, _id: 0 })
         if (userBalance.balance< req.body.amount) {
             return res.send({ status: false, msg: "the user doesn't have enough balance" })
--- src/middlewares/commonMiddlewares.js
+++ src/middlewares/commonMiddlewares.js
@@ -1,19 +1,21 @@
 
 const mid1 = function (req, res, next) {
-    let head = req.headers
     if (!req.headers["isfreeappuser"]) {
         res.send({ status: false, msg: "the request is missing a mandatory header" })
         return;
     }
     else {
-        req.isFreeAppUser = Boolean(req.headers.isfreeappuser)
         let data = req.body
-        data["isFreeAppUser"] = req.isFreeAppUser
-        next()
+        if (req.headers.isfreeappuser == "false"){
+            data["isFreeAppUser"] = false
+            next()
+        }
+        else {
+            data["isFreeAppUser"] = true
+            next()
+        }        
     }
 }
-
-
 
 module.exports.mid1 = mid1
 
